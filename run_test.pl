281fd6f9a0e8d8919d4f6e54d3076b55d383fa37
lucarin91 <lucarin91@gmail.com>
2015-11-22 01:30:12 +0100

    update the satistics0

--- run_test.pl
+++ run_test.pl
@@ -3,7 +3,7 @@ print "V3\n";
 for (my $i = 100; $i <= 1000; $i += 100) {
   my $tot = 0;
   for my $j (1..5){
-    my $msg = `./main_statistics_v3.out -n $i -c 0.1 -s 5000 -r 2423433334`;
+    my $msg = `./main_statistics_v3.out -n $i -c 0.1 -s 1000000 -r 2423433334`;
     $tot += $msg;
     #print "$i\t$msg";
   }
@@ -15,7 +15,7 @@ print "V2\n";
 for (my $i = 100; $i <= 1000; $i += 100) {
   my $tot = 0;
   for my $j (1..5){
-    my $msg = `./main_statistics_v3.out -n $i -c 0.1 -s 5000 -r 2423433334`;
+    my $msg = `./main_statistics_v3.out -n $i -c 0.1 -s 1000000 -r 2423433334`;
     $tot += $msg;
     #print "$i\t$msg";
   }
